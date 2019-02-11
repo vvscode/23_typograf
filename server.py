cfba11ec15ac433e1dfc10377bc30b886ca65bd5
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-02-11 18:45:40 +0400

    Add some tests for further code

--- server.py
+++ server.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def replace_quotes(text):
+    # замена кавычек ' и " на « »
+    return text
+
+
+def replace_hyphen_with_dash_in_text(text):
+    # в нужных местах заменить дефисы на тире
+    return text
+
+
+def replace_hyphen_with_short_dash_in_phones(text):
+    # замена дефисов на короткое тире в номерах телефонов
+    return text
+
+
+def connect_numbers_and_words(text):
+    # связывание чисел с последующими словами неразрывным пробелом
+    return text
+
+def clear_whitespaces(text):
+    # удаление лишних пробелов и переносов строк
+    return text
+
+def connect_unions_and_words(text):
+    # связывание союзов и любых слов из 1-2 символов с последующими словами
+    return text
+
+
+def beautify(text):
+    if not text:
+        return ''
+
+    beautified_text = replace_quotes(text)
+    beautified_text = replace_hyphen_with_dash_in_text(beautified_text)
+    beautified_text = replace_hyphen_with_short_dash_in_phones(beautified_text)
+    beautified_text = connect_numbers_and_words(beautified_text)
+    beautified_text = clear_whitespaces(beautified_text)
+    beautified_text = connect_unions_and_words(beautified_text)
+    return beautified_text
+
+@app.route('/', methods=['POST', 'GET'])
 def form():
-    return render_template('form.html')
+    input = request.form['text'] or '' if request.method == 'POST' else ''
+    result = beautify(input)
+    print({input, result})
+
+    return render_template('form.html', input=input, result=result)
+
 
 if __name__ == "__main__":
     app.run()
---
+++ tests.py
@@ -0,0 +1,76 @@
+import unittest
+from server import replace_quotes, replace_hyphen_with_dash_in_text, \
+    replace_hyphen_with_short_dash_in_phones, connect_numbers_and_words, clear_whitespaces, \
+    connect_unions_and_words
+
+
+class TestTextBeautifier(unittest.TestCase):
+
+    def test_replace_quotes1(self):
+        self.assertEqual(replace_quotes('"1234"'), '«1234»')
+
+    def test_replace_quotes2(self):
+        self.assertEqual(replace_quotes("'1234'"), '«1234»')
+
+    def test_replace_quotes3(self):
+        self.assertEqual(replace_quotes('"1234" 56 \'7\' 8'), '«1234» 56 «7» 8')
+
+    def test_replace_hyphen_with_dash_in_text1(self):
+        self.assertEqual(replace_hyphen_with_dash_in_text('Apple - is a fruit'),
+                         'Apple — is a fruit')
+
+    def test_replace_hyphen_with_dash_in_text2(self):
+        self.assertEqual(replace_hyphen_with_dash_in_text('Apple -- is a fruit'),
+                         'Apple — is a fruit')
+
+    def test_replace_hyphen_with_dash_in_text3(self):
+        self.assertEqual(replace_hyphen_with_dash_in_text('Apple- is a fruit'),
+                         'Apple — is a fruit')
+
+    def test_replace_hyphen_with_dash_in_text4(self):
+        self.assertEqual(replace_hyphen_with_dash_in_text('Apple -is a fruit'),
+                         'Apple — is a fruit')
+
+    def test_replace_hyphen_with_short_dash_in_phones1(self):
+        self.assertEqual(replace_hyphen_with_short_dash_in_phones('123-456-789'),
+                         '123–456–789')
+
+    def test_replace_hyphen_with_short_dash_in_phones2(self):
+        self.assertEqual(replace_hyphen_with_short_dash_in_phones('123 456-789'),
+                         '123 456–789')
+
+    def test_connect_numbers_and_words1(self):
+        self.assertEqual(connect_numbers_and_words('1 apple'), '1&nbsp;apple')
+
+    def test_connect_numbers_and_words2(self):
+        self.assertEqual(connect_numbers_and_words('23 maps'), '23&nbsp;maps')
+
+    def test_connect_numbers_and_words3(self):
+        self.assertEqual(connect_numbers_and_words('0 apples'), '0&nbsp;apples')
+
+    def test_clear_whitespaces1(self):
+        self.assertEqual(clear_whitespaces('I  am'), 'I am')
+
+    def test_clear_whitespaces2(self):
+        self.assertEqual(clear_whitespaces('We are   the heroes  '), 'We are the heroes ')
+
+    def test_clear_whitespaces3(self):
+        self.assertEqual(clear_whitespaces('I need\n\nYour  help'), 'I need\nYour help')
+
+    def test_clear_whitespaces4(self):
+        self.assertEqual(clear_whitespaces('I need\n \nYour  help'), 'I need\nYour help')
+
+    def test_connect_unions_and_words1(self):
+        self.assertEqual(connect_unions_and_words('the dog'), 'the dog')
+
+    def test_connect_unions_and_words2(self):
+        self.assertEqual(connect_unions_and_words('an apple'), 'an&nbsp;apple')
+
+    def test_connect_unions_and_words3(self):
+        self.assertEqual(connect_unions_and_words('an apple'), 'an&nbsp;apple')
+
+    def test_connect_unions_and_words4(self):
+        self.assertEqual(connect_unions_and_words('it is my dog'), 'it&nbsp;is&nbsp;my&nbsp;dog')
+
+if __name__ == '__main__':
+    unittest.main()
